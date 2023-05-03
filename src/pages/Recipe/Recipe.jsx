0d47e9b19c984df53a70799f151a208678143be1
marsrian <marsrian40@gmail.com>
2023-05-03 19:26:39 +0600

    recipe section ingredients added

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -3,7 +3,7 @@ import { Rating } from '@smastrom/react-rating';
 import toast, { Toaster } from 'react-hot-toast';
 
 const Recipe = ({recipe}) => {
-    const {name, cooking_method, rating} = recipe;
+    const {name, cooking_method, rating, ingredients} = recipe;
     const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
     const handleFavourite = () =>{
@@ -14,6 +14,17 @@ const Recipe = ({recipe}) => {
     return (
         <div className='border-2 border-gray-300 p-4'>
             <h3 className='text-xl font-medium mb-2'>{name}</h3>
+            <div>
+                <p className='font-medium mb-2'>Ingredients:</p>
+                <ul className='mb-3'>
+                    {
+                        ingredients.map((ingredient, index)=> <li
+                            className='pl-3'
+                            key={index}
+                        >✅ {ingredient},</li>)
+                    }
+                </ul>
+            </div>
             <p><span className='font-medium mb-2'>Cooking Method:</span> {cooking_method}</p>
             <div className='flex'>
                 <p className='mb-3'><span className='font-medium'>Rating: </span></p>
