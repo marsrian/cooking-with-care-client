76cd402037a53beae6e06fc67fcfa7f559629c53
marsrian <marsrian40@gmail.com>
2023-05-03 11:27:35 +0600

    environment variable complete

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('environment variables ', import.meta.env.VITE_apiKey)
 const firebaseConfig = {
-  apiKey: "AIzaSyCwTjZLr50bIPG4WJ1IzON71AjV_9aig5E",
-  authDomain: "cooking-with-care.firebaseapp.com",
-  projectId: "cooking-with-care",
-  storageBucket: "cooking-with-care.appspot.com",
-  messagingSenderId: "410263328458",
-  appId: "1:410263328458:web:aae0d640df994af64c0633"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
